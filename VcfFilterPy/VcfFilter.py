73f8922040e47e51df45462777c708d2e97ca582
david <dross78375@gmail.com>
2017-04-16 13:42:51 +0100

    mandatory (CHROM, POS, ID etc.) fields can now be filtered

--- VcfFilterPy/VcfFilter.py
+++ VcfFilterPy/VcfFilter.py
@@ -46,18 +46,20 @@ def filter_vcf(vcf, conditions, how='any'):
             handle_headers(line)
         
         if not line.startswith("#"):
-            
+             mand_dict = get_mand_dict(line)
+             mand_values = get_mand_values(conditions, mand_dict)
+
              info_dict = get_info_dict(line)
              info_values = get_info_values(conditions, info_dict)
              
              gt_conditions = [cond for cond in conditions 
-                              if not re.sub(r"\[.*\]", "", cond.split(" ")[0]) in info_dict.keys()]
+                              if not re.sub(r"\[.*\]", "", cond.split(" ")[0]) in info_dict.keys() and not cond.split(" ")[0] in mand_dict.keys()]
              
              # store the genotype values to be tested for each sample in the vcf within a nested list
              gt_values = get_genotype_value(line, gt_conditions)
 
              # combine gt and info values ONLY WORKS IN PYTHON 3.5
-             values = [{**info_values, **x} for x in gt_values]
+             values = [{**info_values, **mand_values, **x} for x in gt_values]
              
              # test all genotype values in all samples for parsed conditions
              tested_samples =  [check_condition(conditions, x) for x in values]
@@ -73,8 +75,25 @@ def filter_vcf(vcf, conditions, how='any'):
 
  
 def handle_headers(line):
-    if line.startswith("##INFO"):
-        pass
+    pass
+
+def get_mand_dict(line):
+    mandatory = line.split("\t")[:7]
+    header = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER']
+    mand_dict = {x:y for x, y in zip(header, mandatory)}
+    return mand_dict
+
+
+def get_mand_values(conditions, man_dict):
+    all_values = {}
+
+    for cond in conditions:
+        field = cond.split(" ")[0]
+        if field in man_dict.keys():
+            v = man_dict.get(field)
+            all_values[field] = v
+
+    return all_values
 
 
 def get_info_dict(line):
@@ -246,10 +265,10 @@ filter_vcf(f, ['GT = 1/1', 'DP > 100', 'AB > 0', 'AC < 50', 'DEPTH = 20']) # == 
 f = '/home/david/projects/pdVCF/test/vcfs/testing3.vcf'
 filter_vcf(f, ['GT = 1/1', 'DP > 100']) # == 257
 filter_vcf(f, ['GT = 0/1', 'DP >= 50', 'GQ >= 30',  'DEPTH > 50']) # == 1896
+filter_vcf(f, ['GT = 0/1', 'DP >= 50', 'GQ >= 30', 'AC[0] > 20', 'CHROM = 1', 'POS > 2235893', 'ID != .']) # == 3
 
 f = '/home/david/projects/pdVCF/test/vcfs/testing4.vcf'
 filter_vcf(f, ['GT = 0/1', 'DP >= 50', 'AC[0] > 20', 'GQ >= 30']) # == 60
 filter_vcf(f, ['GT = 0/1', 'DP >= 50', 'AC[0] > 20', 'GQ >= 30', 'AD[1] >= 20']) # == 49
-
 
 
